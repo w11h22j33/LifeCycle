e59252804a02643ffb2bb4ebbc4a71a837622584
wanghaijun <wanghaijun@umpay.com>
2014-03-11 18:20:50 +0800

    modify

--- LifeCycle/ZYAppDelegate.m
+++ LifeCycle/ZYAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"启动但是还未进入状态保存");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"首次启动但是还未进入状态保存");
     
     return YES;
     
@@ -22,20 +22,20 @@
 {
     // Override point for customization after application launch.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"启动基本完成准备开始运行");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"首次启动基本完成准备开始运行");
     
     return YES;
 }
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application{
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"程序已载入");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"程序已载入");
     
 }
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"处理调用传入的URL");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"处理调用传入的URL");
     
     return YES;
     
@@ -45,7 +45,7 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"应用程序进入活动状态");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"每次启动应用程序进入活动状态");
 
     
 }
@@ -55,7 +55,7 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"应用程序即将进入非活动状态");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"每次应用程序即将进入非活动状态");
     
 }
 
@@ -63,7 +63,7 @@
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"应用程序即将进入前台");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"恢复应用程序即将进入前台");
     
 }
 
@@ -72,7 +72,7 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"应用程序退到后台");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"应用程序退到后台");
     
 }
 
@@ -80,7 +80,7 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     
-    NSLog(@"%s-->\n\t%@",__FUNCTION__,@"应用程序即将退出");
+    NSLog(@"%s-->\n\t\t\t\t\t\t\t\t\t%@",__FUNCTION__,@"应用程序即将退出");
     
 }
 
